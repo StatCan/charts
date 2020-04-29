b38f0b9a9ed68713d670acd0a3f6eec7db585369
William Hearn <sylus1984@gmail.com>
2020-04-28 22:25:46 -0400

    feat(lint): Ensure running chart-testing

--- test/lint.sh
+++ test/lint.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 docker run --rm \
            --volume "$(pwd):/workdir" \
            --workdir /workdir \
-           "quay.io/helmpack/chart-testing:v2.0.1" ct lint --config test/config.yaml --all
+           "quay.io/helmpack/chart-testing:v2.4.1" ct lint --config test/config.yaml --all
