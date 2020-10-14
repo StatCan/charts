e7a4596a8b7cdfffa4f9fc72c81bea0697f08133
Zachary Seguin <zachary@zacharyseguin.ca>
2020-10-14 14:07:02 -0400

    feat(test): Add optional parameter to lint.sh to run link for specific charts only

--- test/lint.sh
+++ test/lint.sh
@@ -2,7 +2,14 @@
 
 set -euxo pipefail
 
+CHARTS="$@"
+if [ -z "$CHARTS" ]; then
+  CHARTS="--all"
+else
+  CHARTS="--charts $CHARTS"
+fi
+
 docker run --rm \
            --volume "$(pwd):/workdir" \
            --workdir /workdir \
-           "quay.io/helmpack/chart-testing:v2.4.1" ct lint --config test/config.yaml --lint-conf test/lintconf.yaml --all
+           "quay.io/helmpack/chart-testing:v2.4.1" ct lint --config test/config.yaml --lint-conf test/lintconf.yaml $CHARTS
